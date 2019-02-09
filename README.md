b0eedf0358c9a4a8960461e616f5fb0301befe95
Jack Franklin <jackfranklin@rmsel.org>
2019-02-09 11:54:07 -0800

    Worked on starting things

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # Rmsel
 The first RMSEL game.
-This line is a test line if you are reading it then delete it.
-Fish
-AHHHHHHHHHHHHHHHHHHHHHHH
--- main.lua
+++ main.lua
@@ -1,1 +1,21 @@
 --Alright
+require 'Settings'
+require 'Push'
+
+
+
+function love.load() 
+	love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
+end
+
+function love.update(dt)
+	if love.keyboard.isDown('escape') then
+		love.event.quit()
+	end
+end
+
+function love.draw()
+	love.graphics.clear(0, 0, 1)
+	love.graphics.setColor(0, 0, 0)
+	love.graphics.print('Your Mom Lol', WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 0, 1, 1)
+end
