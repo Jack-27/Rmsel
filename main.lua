e641a1e9831e6c13cc53ad2f2e9285739b77675d
Jack Franklin <jackfranklin@rmsel.org>
2019-02-09 15:32:04 -0800

    State Machine up and running

--- main.lua
+++ main.lua
@@ -1,18 +1,38 @@
 --Alright
+Push = require 'Push'
+Class = require 'Class'
+
+
 require 'Settings'
-require 'Push'
+require 'Fighter'
+require 'StateMachine'
+
+require 'states/BaseState'
+require 'states/TitleState'
 
 
 
 function love.load() 
 	love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
+
+	gStateMachine = StateMachine {
+        ['title'] = function() return TitleState() end
+    }
+    gStateMachine:change('title')
+        
+
+
+
+
 end
 
 function love.update(dt)
+
+	gStateMachine:update(dt)
 	if love.keyboard.isDown('escape') then
 		love.event.quit()
 	end
 end
 function love.draw()
-
+	gStateMachine:render()
 end
--- states/TitleState.lua
+++ states/TitleState.lua
@@ -1,1 +1,23 @@
 TitleState = Class{__includes = BaseState}
+
+function TitleState:init() end
+
+
+function TitleState:enter() end
+
+
+function TitleState:exit() end
+
+
+function TitleState:update(dt)
+	if love.keyboard.isDown('return') then
+		gStateMachine:change()
+	end
+end
+
+
+function TitleState:render()
+	love.graphics.clear(0, 0, .75, 1)
+
+
+end
